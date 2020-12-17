348f754eedd4bb63c1e560ce5d0e3225c4417a25
hamzahix <hred775@gmail.com>
2020-12-17 12:28:21 +0000

    first_architecturePattern_MVC v1

---
+++ app/src/main/java/com/hixman/first_architecturepattern/ui/MainActivity.java
@@ -0,0 +1,51 @@
+package com.hixman.first_architecturepattern.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.hixman.first_architecturepattern.model.MVC_model;
+import com.hixman.first_architecturepattern.R;
+
+// this is how MVC works in a simple way,
+// it's main that the MainActivity is the controller and you have to do everything yourself
+/**
+ * MVC:
+ * M:model      ==> MVC_model
+ * V:view       ==> tv_data
+ * C:controller ==> MainActivity
+ * */
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView tv_data ;
+    Button btn_getData;
+    Button btn_goToMvp;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // here we find and define our views
+        tv_data= findViewById(R.id.tv_data);
+        btn_getData= findViewById(R.id.btn_getdata);
+        btn_goToMvp = findViewById(R.id.btn_gotomvp);
+
+    }
+    public MVC_model getDataFromDataBase(){
+        return new MVC_model("hix coder","15/1/2020","hello everyone",1);
+    }
+
+    public void Buttons_mainActivity(View view) {
+        if (btn_getData.equals(view)) {
+            tv_data.setText(getDataFromDataBase().getName());
+        } else if (btn_goToMvp.equals(view)) {
+            Toast.makeText(getApplicationContext(), "we dont have this feature yet", Toast.LENGTH_LONG).show();
+        }
+    }
+}
